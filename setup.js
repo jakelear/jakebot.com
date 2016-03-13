8a94a5b795378c44986fa0f1c625f694242a1336
Jake Lear <jake.lear@voxmedia.com>
2016-03-13 18:02:08 -0400

    DRY up serve/deploy

---
+++ setup.js
@@ -0,0 +1,67 @@
+// Setup metalsmith plugins
+var Metalsmith  = require('metalsmith'),
+    markdown    = require('metalsmith-markdown'),
+    permalinks  = require('metalsmith-permalinks'),
+    branch      = require('metalsmith-branch'),
+    collections = require('metalsmith-collections'),
+    excerpts    = require('metalsmith-excerpts'),
+    layouts     = require('metalsmith-layouts'),
+    assets      = require('metalsmith-assets'),
+    sass        = require('metalsmith-sass'),
+    ignore      = require('metalsmith-ignore'),
+    serve       = require('metalsmith-serve'),
+    watch       = require('metalsmith-watch');
+
+// Non metalsmith-specific modules
+var nunjucks    = require('nunjucks'),
+    date        = require('nunjucks-date');
+
+// Basic metalsmith config
+var config = {
+  source_dir      : './src/content',
+  build_dir       : './build',
+  layouts_dir     : './src/layouts',
+  layouts_engine  : 'nunjucks',
+  assets: {
+    source: './src/content/assets',
+    destination: './assets'
+  }
+};
+
+// Metadata gets passed through to site
+var metadata = {
+  title: 'Jakebot',
+  url: 'http://jakebot.com',
+  twitter_user: '@jakelear',
+  fb_admin: '15601180'
+};
+
+// Template configuration
+nunjucks
+  .configure('./src/layouts')
+  .addFilter('date', date);
+
+// Date formatter
+date
+  .setDefaultFormat('MMMM Do YYYY');
+
+module.exports = {
+  metalsmith: Metalsmith(__dirname)
+  .metadata(metadata)
+  .use(markdown())
+  .source(config.source_dir)
+  .destination(config.build_dir)
+  .use(sass({
+    outputDir: 'css/'
+  }))
+  .use(
+    layouts({
+      engine: config.layouts_engine,
+      directory: config.layouts_dir
+    })
+  )
+  .use(ignore([
+    'styles/**/*'
+  ]))
+  .use(assets(config.assets))
+}
