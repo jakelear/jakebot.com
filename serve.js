2755d700106dcd6d556d28be4e010788d8cb5196
Jake Lear <jake.lear@voxmedia.com>
2016-05-07 15:09:30 -0400

    Do away with webfonts

--- serve.js
+++ serve.js
@@ -1,10 +1,15 @@
 var setup       = require('./setup'),
-    serve       = require('metalsmith-serve');
+    serve       = require('metalsmith-serve'),
+    browsersync = require('metalsmith-browser-sync');
 
 // SERVE
 // TODO: Add some sort of watch
 setup.metalsmith
   .use(serve())
+  .use(browsersync({
+    server : "build",
+    files  : ["src/**/*"]
+  }))
   .build(function (err) {
     if (err) {
       console.log(err)
